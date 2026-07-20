a34de0bc1cd0b60ea6802bd654d33bd56d3f4bc5
divyaroshini7 <divyaroshinip.ai2023@dce.edu.in>
2026-07-20 13:25:35 +0530

    Time: 11 ms (16.67%), Space: 19.2 MB (90.88%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = list(map(str, nums))
+
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] < nums[j] + nums[i]:
+                    nums[i], nums[j] = nums[j], nums[i]
+
+        return "0" if nums[0] == "0" else "".join(nums)
